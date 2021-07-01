cf65260814dc9315ce95ac1f74df2dd6eef0539b
Weijia Sun <swj1442291549@gmail.com>
2021-07-01 13:24:06 +0800

    Fix download_and_save_file

--- download.py
+++ download.py
@@ -1,29 +1,30 @@
 from pathlib import Path
+import sys
 import requests
 
 
 def download_and_save_file(file_name, url, folder='.'):
     if not Path(folder, file_name).is_file():
-        print("Download {0} ...".format(file_name))
         p = Path(folder)
         if not p.is_dir():
             p.mkdir(parents=True)
 
-        print("Downloading {0} ...".format(file_name))
-        response = requests.get(url, stream=True)
-        total_length = response.headers.get('content-length')
+        with open(Path(folder, file_name), "wb") as f:
+            print("Downloading {0} ...".format(file_name))
+            response = requests.get(url, stream=True)
+            total_length = response.headers.get('content-length')
 
-        if total_length is None: # no content length header
-            f.write(response.content)
-        else:
-            dl = 0
-            total_length = int(total_length)
-            for data in response.iter_content(chunk_size=4096):
-                dl += len(data)
-                f.write(data)
-                done = int(50 * dl / total_length)
-                sys.stdout.write("\r[%s%s]" % ('=' * done, ' ' * (50-done)) )    
-                sys.stdout.flush()
+            if total_length is None: # no content length header
+                f.write(response.content)
+            else:
+                dl = 0
+                total_length = int(total_length)
+                for data in response.iter_content(chunk_size=4096):
+                    dl += len(data)
+                    f.write(data)
+                    done = int(50 * dl / total_length)
+                    sys.stdout.write("\r[%s%s]" % ('=' * done, ' ' * (50-done)) )    
+                    sys.stdout.flush()
 
 
 
